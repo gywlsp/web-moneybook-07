14b8a6872615cce611c5963cd3e4974dfcf44015
gywlsp <gywls517@gmail.com>
2022-07-18 22:34:04 +0900

    config :wrench: prettier, eslint 설정 추가

--- src/app.js
+++ src/app.js
@@ -1,34 +1,35 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-const webpack = require('webpack')
-const middleware = require('webpack-dev-middleware')
-const webpackConfig = require('../webpack.config')
-const compiler = webpack(webpackConfig)
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
+const webpackConfig = require('../webpack.config.js');
 
-var indexRouter = require('./backend/routes/index')
-var usersRouter = require('./backend/routes/users')
+const compiler = webpack(webpackConfig);
 
-var app = express()
+const indexRouter = require('./backend/routes/index.js');
+const usersRouter = require('./backend/routes/users.js');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+const app = express();
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'development') {
   // 웹팩 설정
-  app.use('/', middleware(compiler, {}))
+  app.use('/', middleware(compiler, {}));
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'frontend/public/dist')))
+  app.use(express.static(path.resolve(__dirname, 'frontend/public/dist')));
 }
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-module.exports = app
+module.exports = app;
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,6 @@
-import './stylesheets/style.css'
-import closeIcon from './assets/closeIcon.svg'
+import './stylesheets/style.css';
+import closeIcon from './assets/closeIcon.svg';
 
-const $img = document.createElement('img')
-$img.src = closeIcon
-document.querySelector('body').appendChild($img)
+const $img = document.createElement('img');
+$img.src = closeIcon;
+document.querySelector('body').appendChild($img);
