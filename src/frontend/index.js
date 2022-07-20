7a0d8b7b49799467db17e766da5e96eee823864d
gywlsp <gywls517@gmail.com>
2022-07-20 18:37:23 +0900

    feat :sparkles: globalStore 초기 설정 코드 추가

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,30 @@
 import './stylesheets/style.css';
-import closeIcon from './assets/closeIcon.svg';
+import './stylesheets/global.css';
 
+import globalStore from './stores/global.js';
+
+import closeIcon from './assets/closeIcon.svg';
+const $app = document.createElement('div');
+$app.id = 'app';
+document.querySelector('body').appendChild($app);
+
+const currentDate = new Date();
+
+globalStore.init('selectedTab', 'detail');
+
+globalStore.init('globalState', {
+  year: currentDate.getFullYear(),
+  month: currentDate.getMonth() + 1,
+});
+
+globalStore.init('detailState', {
+  income: true,
+  expenditure: true,
+});
+
+globalStore.init('statisticsState', {
+  categoryId: null,
+});
 const $img = document.createElement('img');
 $img.src = closeIcon;
 document.querySelector('body').appendChild($img);
