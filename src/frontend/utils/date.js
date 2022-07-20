119779f157253641d1a0cd7167e3ce4df923cf06
gywlsp <gywls517@gmail.com>
2022-07-20 18:20:31 +0900

    feat :sparkles: getPrevMonth, getNextMonth util 함수 추가

---
+++ src/frontend/utils/date.js
@@ -0,0 +1,13 @@
+export const getPrevMonth = ({year, month}) => {
+  if (month === 1) {
+    return {year: year - 1, month: 12};
+  }
+  return {year, month: month - 1};
+};
+
+export const getNextMonth = ({year, month}) => {
+  if (month === 12) {
+    return {year: year + 1, month: 1};
+  }
+  return {year, month: month + 1};
+};
