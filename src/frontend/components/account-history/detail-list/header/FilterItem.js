a72ef00f3fa34d15116df28177a44b84c11f8485
gywlsp <gywls517@gmail.com>
2022-07-21 20:38:59 +0900

    feat :sparkles: AccountHistoryDetailListHeaderFilterItem 추가

---
+++ src/frontend/components/account-history/detail-list/header/FilterItem.js
@@ -0,0 +1,39 @@
+import globalStore from '../../../../stores/global.js';
+
+import checkIcon from '../../../../assets/check.svg';
+
+export default class AccountHistoryDetailListHeaderFilterItem {
+  constructor({$parent, model, state}) {
+    this.$target = document.createElement('div');
+    this.$target.classList.add('history-detail-list-filter-item');
+    $parent.appendChild(this.$target);
+
+    this.detailModel = model;
+    this.state = state;
+    this.render();
+    this.handleEvent();
+  }
+
+  handleEvent() {
+    this.$target.addEventListener('click', () => {
+      const {name, checked} = this.state;
+      const detailState = globalStore.get('detailState');
+      globalStore.set('detailState', {...detailState, [name]: !checked});
+    });
+  }
+
+  render() {
+    const {name, total, checked} = this.state;
+    this.$target.innerHTML = `
+        <input type="checkbox" id=${name} name=${name}/>
+        <label for="${name}">
+            <span class="${checked ? 'checked' : ''}">
+                <img src="${checkIcon}" alt="check-icon"/>
+            </span>
+            <p>
+                ${name === 'income' ? '수입' : '지출'} ${total.toLocaleString()}
+            </p>
+        </label>
+    `;
+  }
+}
