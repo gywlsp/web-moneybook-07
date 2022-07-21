f80f079e420ae76d7e45cde7693eab349c41ec2f
gywlsp <gywls517@gmail.com>
2022-07-21 16:34:44 +0900

    feat :sparkles: getTodayDateString date util 함수 추가

--- src/frontend/utils/date.js
+++ src/frontend/utils/date.js
@@ -13,3 +13,12 @@ export const getNextMonth = ({year, month}) => {
 };
 
 export const padZero = num => num.toString().padStart(2, '0');
+
+export const getTodayDateString = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  const result = `${year}${padZero(month)}${padZero(date)}`;
+  return result;
+};
