0a28b52f222fb4567da811143edc7d7fcd1d2aab
gywlsp <gywls517@gmail.com>
2022-07-21 16:58:20 +0900

    feat :sparkles: fe index.js 파일에서 AccountHistoryDetailModel, AccountHistoryDetailView 인스턴스 생성

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,20 +1,23 @@
 import './stylesheets/style.css';
 import './stylesheets/global.css';
+import './stylesheets/detail.css';
 
 import globalStore from './stores/global.js';
+import AccountHistoryDetailModel from './models/account-history/DetailModel.js';
 
 import GlobalHeader from './components/GlobalHeader.js';
+import AccountHistoryDetailView from './views/account-history/DetailView.js';
 
 const $app = document.createElement('div');
 $app.id = 'app';
 document.querySelector('body').appendChild($app);
 
 const getSelectedTab = () => {
-  const { pathname } = window.location
+  const {pathname} = window.location;
   if (pathname === '/calendar') return 'calendar';
-  if (pathname === '/statistics') return 'statistics'
+  if (pathname === '/statistics') return 'statistics';
   return 'detail';
-}
+};
 
 const currentDate = new Date();
 
@@ -34,9 +37,12 @@ globalStore.init('statisticsState', {
   categoryId: null,
 });
 
-window.addEventListener("popstate", () => {
-  globalStore.set("selectedTab", getSelectedTab())
-})
+window.addEventListener('popstate', () => {
+  globalStore.set('selectedTab', getSelectedTab());
+});
 
 // eslint-disable-next-line no-unused-vars
 const globalHeader = new GlobalHeader();
+
+const detailModel = new AccountHistoryDetailModel();
+const detailView = new AccountHistoryDetailView({model: detailModel});
