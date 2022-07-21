375c3bba79b277b64c9289f27f682c4049e456b2
gywlsp <gywls517@gmail.com>
2022-07-21 22:05:22 +0900

    feat :sparkles: AccountHistoryDetailListByDate 추가

---
+++ src/frontend/components/account-history/detail-list/by-date/index.js
@@ -0,0 +1,31 @@
+import AccountHistoryDetailListByDateHeader from './header.js';
+import AccountHistoryDetailListByDateDetail from './detail.js';
+
+export default class AccountHistoryDetailListByDate {
+  constructor({$parent, model, state}) {
+    this.$target = document.createElement('div');
+    this.$target.classList.add('history-detail-list-by-date');
+
+    $parent.appendChild(this.$target);
+    this.detailModel = model;
+    this.state = state;
+
+    this.render();
+  }
+
+  render() {
+    const {dateString, totalIncome, totalExpenditure, details} = this.state.date;
+    new AccountHistoryDetailListByDateHeader({
+      $parent: this.$target,
+      model: this.detailModel,
+      state: {dateString, totalIncome, totalExpenditure},
+    });
+    details.forEach(detail => {
+      new AccountHistoryDetailListByDateDetail({
+        $parent: this.$target,
+        model: this.detailModel,
+        state: detail,
+      });
+    });
+  }
+}
