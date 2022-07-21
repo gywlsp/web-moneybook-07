c8218cf4ab31833d3e7945d74d2657987f6d951a
gywlsp <gywls517@gmail.com>
2022-07-22 00:54:28 +0900

    feat :sparkles: AccountHistoryAPI 객체에 put 메서드 추가

--- src/frontend/api/history.js
+++ src/frontend/api/history.js
@@ -6,6 +6,8 @@ const AccountHistoryAPI = {
     const queryString = getQueryString(filter);
     return API.get(`/api/account-history${queryString}`);
   },
+  post: detail => API.post('/api/account-history', detail),
+  put: (id, detail) => API.put(`/api/account-history/${id}`, detail),
 };
 
 export default AccountHistoryAPI;
