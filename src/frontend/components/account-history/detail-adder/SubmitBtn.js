2736ff2c780c88745005f263317a7fed94f0b0ba
gywlsp <gywls517@gmail.com>
2022-07-21 18:55:17 +0900

    feat :sparkles: AccountHistoryDetailAdderSubmitBtn 추가

---
+++ src/frontend/components/account-history/detail-adder/SubmitBtn.js
@@ -0,0 +1,18 @@
+import checkIcon from '../../../assets/check.svg';
+
+export default class AccountHistoryDetailAdderSubmitBtn {
+  constructor({$parent, model}) {
+    this.$target = document.createElement('button');
+    this.$target.classList.add('history-detail-adder-submitBtn');
+    this.$target.disabled = true;
+    $parent.appendChild(this.$target);
+
+    this.detailModel = model;
+
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = `<img src="${checkIcon}" alt="check-icon" />`;
+  }
+}
