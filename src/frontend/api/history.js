e99fed98bd17a0072ae25cc5aa35a5d93f6d04a5
gywlsp <gywls517@gmail.com>
2022-07-22 09:38:49 +0900

    feat :sparkles: AccountHistoryAPI에 getCategories, getPayments 메서드 추가

--- src/frontend/api/history.js
+++ src/frontend/api/history.js
@@ -8,6 +8,11 @@ const AccountHistoryAPI = {
   },
   post: detail => API.post('/api/account-history', detail),
   put: (id, detail) => API.put(`/api/account-history/${id}`, detail),
+  getCategories: (filter = {}) => {
+    const queryString = getQueryString(filter);
+    return API.get(`/api/account-history/categories${queryString}`);
+  },
+  getPayments: () => API.get('/api/account-history/payments'),
 };
 
 export default AccountHistoryAPI;
