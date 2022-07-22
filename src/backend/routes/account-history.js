708fb5e394519e3610c222546f8103095cb6e59d
gywlsp <gywls517@gmail.com>
2022-07-22 22:27:06 +0900

    feat :sparkles: AccountHistoryRouter 업데이트

--- src/backend/routes/account-history.js
+++ src/backend/routes/account-history.js
@@ -4,9 +4,11 @@ const AccountHistoryController = require('../controllers/account-history.js');
 const router = express.Router();
 
 router.get('/', AccountHistoryController.getList);
-router.post('/', AccountHistoryController.createHistory)
-router.put('/:id', AccountHistoryController.updateHistory)
-router.get('/categories', AccountHistoryController.getCategories)
-router.get('/payments', AccountHistoryController.getPayments)
+router.post('/', AccountHistoryController.createHistory);
+router.put('/:id', AccountHistoryController.updateHistory);
+router.get('/categories', AccountHistoryController.getCategories);
+router.get('/payments', AccountHistoryController.getPayments);
+router.post('/payments', AccountHistoryController.createPayment);
+router.delete('/payments/:id', AccountHistoryController.deletePayment);
 
 module.exports = router;
