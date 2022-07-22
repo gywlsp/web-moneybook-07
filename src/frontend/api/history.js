541a9e40fb743301d30c741fa6e86a6db542614a
padosum <makalu171@gmail.com>
2022-07-22 21:30:12 +0900

    feat ✨ AccountHistoryAPI에 postPayment, deletePayment 메서드 추가

--- src/frontend/api/history.js
+++ src/frontend/api/history.js
@@ -1,18 +1,20 @@
 import API from './index.js';
-import {getQueryString} from '../utils/api.js';
+import { getQueryString } from '../utils/api.js';
 
 const AccountHistoryAPI = {
   getList: filter => {
     const queryString = getQueryString(filter);
     return API.get(`/api/account-history${queryString}`);
   },
-  post: detail => API.post('/api/account-history', detail),
-  put: (id, detail) => API.put(`/api/account-history/${id}`, detail),
+  post: body => API.post('/api/account-history', body),
+  put: (id, body) => API.put(`/api/account-history/${id}`, body),
   getCategories: (filter = {}) => {
     const queryString = getQueryString(filter);
     return API.get(`/api/account-history/categories${queryString}`);
   },
   getPayments: () => API.get('/api/account-history/payments'),
+  postPayment: (body) => API.post('/api/account-history/payments', body),
+  deletePayment: (id) => API.delete(`/api/account-history/payments/${id}`),
 };
 
 export default AccountHistoryAPI;
