f2fa4f2ab569cfd65ba57a28fa8321ad97f62428
padosum <makalu171@gmail.com>
2022-07-22 21:33:18 +0900

    feat ✨ select 컴포넌트에 payment option add/delete 버튼 이벤트 핸들러 추가

--- src/frontend/components/account-history/detail-adder/Select.js
+++ src/frontend/components/account-history/detail-adder/Select.js
@@ -1,7 +1,10 @@
+import { updatePaymentConfirmModal } from '../../../utils/payment.js';
+
+import PaymentConfirmModal from './PaymentConfirmModal.js'
 import closeIcon from '../../../assets/close.svg';
 
 export default class AccountHistoryDetailAdderSelect {
-  constructor({$parent, model, state}) {
+  constructor({ $parent, model, state }) {
     this.$target = document.createElement('div');
     this.$target.classList.add('history-detail-adder-select-wrapper');
     $parent.appendChild(this.$target);
@@ -15,6 +18,12 @@ export default class AccountHistoryDetailAdderSelect {
   }
 
   handleEvent() {
+    const closeSelect = () => {
+      this.$target.querySelector('.select-items').classList.add('hidden');
+      const $selectLabel = this.$target.querySelector('.select-selected');
+      $selectLabel.classList.remove('select-arrow-active');
+    }
+
     this.$target.addEventListener('click', e => {
       const $selectLabel = e.target.closest('.select-selected');
       const $selectedItems = this.$target.querySelector('.select-items');
@@ -24,9 +33,23 @@ export default class AccountHistoryDetailAdderSelect {
         return;
       }
 
+      const $optionAddBtn = e.target.closest('button.select-item');
+      if ($optionAddBtn) {
+        updatePaymentConfirmModal('add');
+        closeSelect()
+        return;
+      }
+
       const $selectItem = e.target.closest('div.select-item');
       if ($selectItem) {
-        const {optionValue: selectedValue} = $selectItem.dataset;
+        const { optionValue: selectedValue, optionTitle } = $selectItem.dataset;
+        const $optionDeleteBtn = e.target.closest('.option-delete-btn');
+        if ($optionDeleteBtn) {
+          updatePaymentConfirmModal('delete', { value: selectedValue, title: optionTitle });
+          closeSelect();
+          return;
+        }
+
         const $select = this.$target.querySelector('.history-detail-adder-select');
         $select.value = selectedValue;
         const event = new Event('input', {
@@ -37,15 +60,12 @@ export default class AccountHistoryDetailAdderSelect {
     });
 
     this.$target.addEventListener('input', e => {
-      const {value} = e.target;
+      const { value } = e.target;
       const $selectLabel = this.$target.querySelector('.select-selected');
-      $selectLabel.classList.remove('select-arrow-active');
       $selectLabel.style.color = '#1e2222';
-
-      const $selectedItems = this.$target.querySelector('.select-items');
-      $selectedItems.classList.add('hidden');
+      closeSelect();
       [...this.$target.querySelectorAll('div.select-item')].forEach($elem => {
-        const {optionValue, optionTitle} = $elem.dataset;
+        const { optionValue, optionTitle } = $elem.dataset;
         if (optionValue === value) {
           $elem.classList.add('same-as-selected');
           $selectLabel.innerHTML = optionTitle;
@@ -57,35 +77,37 @@ export default class AccountHistoryDetailAdderSelect {
   }
 
   render() {
-    const {name} = this.state;
+    const { name } = this.state;
     const {
-      categories: {income, expenditure},
+      categories: { income, expenditure },
       payments,
     } = this.detailModel.getData();
     const options =
       name === 'category'
-        ? [...income.map(v => ({...v, type: 'income'})), ...expenditure.map(v => ({...v, type: 'expenditure'}))]
+        ? [...income.map(v => ({ ...v, type: 'income' })), ...expenditure.map(v => ({ ...v, type: 'expenditure' }))]
         : payments;
-    const optionData = options.map(({id, title, type}) => ({value: id, title, type}));
+    const optionData = options.map(({ id, title, type }) => ({ value: id, title, type }));
     this.$target.innerHTML = `
         <select id="${name}" name="${name}" class="history-detail-adder-select">
-          ${optionData.map(({value, title}) => `<option value="${value}">${title}</option>`).join('')}
+          ${optionData.map(({ value, title }) => `<option value="${value}">${title}</option>`).join('')}
         </select>
         <div class="select-selected">선택하세요</div>
         <div class="select-items hidden">
           ${optionData
-            .map(
-              ({value, title, type}) =>
-                `<div class="select-item ${type === 'income' ? 'hidden' : ''}" ${
-                  type ? `data-option-type="${type}"` : ''
-                } data-option-value="${value}"  data-option-title="${title}">
+        .map(
+          ({ value, title, type }) =>
+            `<div class="select-item ${type === 'income' ? 'hidden' : ''}" ${type ? `data-option-type="${type}"` : ''
+            } data-option-value="${value}"  data-option-title="${title}">
                   ${title}
                   ${name === 'payment' ? `<img class="option-delete-btn" src="${closeIcon}" alt="close-icon"/>` : ''}
                 </div>`,
-            )
-            .join('')}
+        )
+        .join('')}
             ${name === 'payment' ? `<button class="select-item">추가하기</button>` : ``}
         </div>
     `;
+    if (name === 'payment') {
+      new PaymentConfirmModal({ $parent: this.$target, model: this.detailModel })
+    }
   }
 }
