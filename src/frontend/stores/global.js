e3d6b35cc7a3a90482d298492b7a28cf0c04afac
gywlsp <gywls517@gmail.com>
2022-07-23 20:49:30 +0900

    refactor :recycle: globalStore 클로저 활용해 리팩토링

--- src/frontend/stores/global.js
+++ src/frontend/stores/global.js
@@ -1,28 +1,48 @@
-const globalStore = {
-  state: {},
-  init(key, defaultValue) {
-    if (key in this.state) throw Error('이미 존재하는 key값 입니다.');
-    this.state[key] = {
-      state: defaultValue,
-      observers: new Set(),
-    };
-    return key;
-  },
-  get(key) {
-    if (!(key in this.state)) throw Error('존재하지 않는 key값 입니다.');
-    return this.state[key].state;
-  },
-  set(key, newValue) {
-    if (!(key in this.state)) throw Error('존재하지 않는 key값 입니다.');
-    this.state[key].state = newValue;
-    this.notify(key);
-  },
-  subscribe(key, observer) {
-    this.state[key].observers.add(observer);
-  },
-  notify(key) {
-    this.state[key].observers.forEach(observer => observer());
-  },
+const getGlobalStore = () => {
+  const state = {};
+
+  const globalStore = {
+    init(key, defaultValue) {
+      if (key in state) throw Error('이미 존재하는 key값 입니다.');
+      state[key] = {
+        state: defaultValue,
+        observers: new Set(),
+      };
+      return key;
+    },
+    get(key) {
+      if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
+      return state[key].state;
+    },
+    set(key, newValue) {
+      if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
+      state[key].state = newValue;
+      this.notify(key);
+    },
+    subscribe(key, observer) {
+      state[key].observers.add(observer);
+    },
+    notify(key) {
+      state[key].observers.forEach(observer => observer());
+    },
+  };
+
+  const currentDate = new Date();
+  globalStore.init('globalState', {
+    year: currentDate.getFullYear(),
+    month: currentDate.getMonth() + 1,
+  });
+  globalStore.init('detailState', {
+    income: true,
+    expenditure: true,
+  });
+  globalStore.init('statisticsState', {
+    categoryId: null,
+  });
+
+  return globalStore;
 };
 
-export default globalStore;
+const GlobalStore = getGlobalStore();
+
+export default GlobalStore;
