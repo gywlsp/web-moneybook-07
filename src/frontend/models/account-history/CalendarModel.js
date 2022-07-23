62a8e7eec93814ea19e0660c91046f4ee1557871
gywlsp <gywls517@gmail.com>
2022-07-23 21:17:22 +0900

    feat :sparkles: AccountHistoryCalendarModel 추가

---
+++ src/frontend/models/account-history/CalendarModel.js
@@ -0,0 +1,30 @@
+/* eslint-disable class-methods-use-this */
+import Observer from '../index.js';
+import GlobalStore from '../../stores/global.js';
+import AccountHistoryAPI from '../../api/history.js';
+import {padZero} from '../../utils/date.js';
+
+export default class AccountHistoryCalendarModel extends Observer {
+  constructor() {
+    super();
+    this.data = {
+      history: {totalDetailCnt: 0, totalIncome: 0, totalExpenditure: 0, dates: []},
+    };
+    this.setData();
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  fetchHistory() {
+    const {year, month} = GlobalStore.get('globalState');
+    return AccountHistoryAPI.getList({year, month: padZero(month), income: true, expenditure: true});
+  }
+
+  async setData() {
+    const history = await this.fetchHistory();
+    this.data = {history};
+    this.notify();
+  }
+}
