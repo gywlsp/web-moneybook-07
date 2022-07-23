c5957cdbac23cd56b58cc33f55aa0717daa506f9
gywlsp <gywls517@gmail.com>
2022-07-24 00:39:35 +0900

    feat :sparkles: getDateCnt util 함수 추가

--- src/frontend/utils/date.js
+++ src/frontend/utils/date.js
@@ -32,3 +32,19 @@ export const getDateString = ({year, month, date, withDot = true}) => {
   return result;
 };
 
+export const getDateCnt = month => {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 2:
+      return 28;
+    default:
+      return 30;
+  }
+};
