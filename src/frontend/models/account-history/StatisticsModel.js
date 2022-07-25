7fb197c398b8fb9c8611f69b10c84dab8bb237d5
padosum <makalu171@gmail.com>
2022-07-25 19:42:00 +0900

    feat ✨ StatisticsModel 추가

---
+++ src/frontend/models/account-history/StatisticsModel.js
@@ -0,0 +1,37 @@
+/* eslint-disable class-methods-use-this */
+import Observer from '../index.js';
+import Router from '../../Router.js';
+import GlobalStore from '../../stores/global.js';
+import AccountHistoryAPI from '../../api/history.js';
+import { padZero } from '../../utils/date.js';
+
+export default class AccountHistoryStatisticsModel extends Observer {
+    constructor() {
+        super();
+        GlobalStore.subscribe('globalState', () => {
+            if (Router.get('pathname') !== '/statistics') return;
+            GlobalStore.set('statisticsState', { categoryId: null })
+            this.setData.apply(this);
+        });
+        this.data = {
+            categories: { expenditure: [] },
+        };
+        this.setData();
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    fetchCategories() {
+        const { year, month } = GlobalStore.get('globalState');
+        return AccountHistoryAPI.getCategories({ year, month: padZero(month) });
+    }
+
+    async setData() {
+        const { expenditure } = await this.fetchCategories();
+        this.data = { ...this.data, categories: { expenditure: expenditure.sort((a, b) => b.percentage - a.percentage) } };
+        this.notify();
+    }
+
+}
