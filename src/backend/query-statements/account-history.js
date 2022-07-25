ddf13f42a30728b3e10940e6c5833ac7951e1709
gywlsp <gywls517@gmail.com>
2022-07-25 20:40:24 +0900

    feat ✨ queryStatements의 getListQuery 메서드에 categoryId 인자 추가

--- src/backend/query-statements/account-history.js
+++ src/backend/query-statements/account-history.js
@@ -1,5 +1,5 @@
 const queryStatements = {
-  getListQuery: ({year, month}) => `
+  getListQuery: ({year, month, categoryId}) => `
     select ACCOUNT_HISTORY.id as id
      , ACCOUNT_HISTORY.dateString as dateString
      , ACCOUNT_HISTORY.categoryId as categoryId
@@ -15,6 +15,7 @@ const queryStatements = {
     join ACCOUNT_HISTORY_CATEGORY 
     on ACCOUNT_HISTORY.categoryId = ACCOUNT_HISTORY_CATEGORY.id 
     where ACCOUNT_HISTORY.dateString like "${year}.${month}.__"
+    ${categoryId === undefined ? '' : `and ACCOUNT_HISTORY.categoryId = ${categoryId}`}
     order by ACCOUNT_HISTORY.dateString DESC;
     `,
 
