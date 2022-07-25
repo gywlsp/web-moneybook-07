c8392411f03b469e1fc6f9f82d84208380b750c8
gywlsp <gywls517@gmail.com>
2022-07-25 20:44:39 +0900

    feat :sparkles: AccountHistoryStatisticsModel에 fetchHistory, mutateHistory 메서드 추가

--- src/frontend/models/account-history/StatisticsModel.js
+++ src/frontend/models/account-history/StatisticsModel.js
@@ -6,36 +6,47 @@ import AccountHistoryAPI from '../../api/history.js';
 import {padZero} from '../../utils/date.js';
 
 export default class AccountHistoryStatisticsModel extends Observer {
-    constructor() {
-        super();
-        GlobalStore.subscribe('globalState', () => {
-            if (Router.get('pathname') !== '/statistics') return;
+  constructor() {
+    super();
+    GlobalStore.subscribe('globalState', () => {
+      if (Router.get('pathname') !== '/statistics') return;
       GlobalStore.set('statisticsState', {categoryId: null});
-            this.setData.apply(this);
-        });
+      this.setData.apply(this);
+    });
     GlobalStore.subscribe('statisticsState', () => {
       if (Router.get('pathname') !== '/statistics' || GlobalStore.get('statisticsState').categoryId === null) return;
       this.mutateHistory.apply(this);
     });
-        this.data = {
-            categories: { expenditure: [] },
-        };
-        this.setData();
-    }
+    this.data = {
+      categories: {expenditure: [], history: {totalDetailCnt: 0, totalIncome: 0, totalExpenditure: 0, dates: []}},
+    };
+    this.setData();
+  }
 
-    getData() {
-        return this.data;
-    }
+  getData() {
+    return this.data;
+  }
 
-    fetchCategories() {
-        const { year, month } = GlobalStore.get('globalState');
-        return AccountHistoryAPI.getCategories({ year, month: padZero(month) });
-    }
+  fetchCategories() {
+    const {year, month} = GlobalStore.get('globalState');
+    return AccountHistoryAPI.getCategories({year, month: padZero(month)});
+  }
 
-    async setData() {
-        const { expenditure } = await this.fetchCategories();
-        this.data = { ...this.data, categories: { expenditure: expenditure.sort((a, b) => b.percentage - a.percentage) } };
-        this.notify();
-    }
+  fetchHistory() {
+    const {year, month} = GlobalStore.get('globalState');
+    const {categoryId} = GlobalStore.get('statisticsState');
+    return AccountHistoryAPI.getList({year, month: padZero(month), categoryId});
+  }
 
+  async mutateHistory() {
+    const history = await this.fetchHistory();
+    this.data = {...this.data, history};
+    this.notify();
+  }
+
+  async setData() {
+    const {expenditure} = await this.fetchCategories();
+    this.data = {...this.data, categories: {expenditure: expenditure.sort((a, b) => b.percentage - a.percentage)}};
+    this.notify();
+  }
 }
