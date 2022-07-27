be8c347152afbd57f50108ba3e1c827a39d719c9
padosum <makalu171@gmail.com>
2022-07-27 21:34:33 +0900

    chore ⚙️ 카테고리 아이디값 변경

--- src/frontend/constants/colors.js
+++ src/frontend/constants/colors.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 export const CATEGORY_COLORS = {
-    2: '#4a6cc3',
-    3: '#4ca1de',
-    4: '#94d3cc',
-    5: '#4cb8b8',
-    6: '#6ed5eb',
-    7: '#d092e2',
-    8: '#817dce',
-    9: '#b9d58c',
-    10: '#e6d267',
-    11: '#e2b765',
+    1: '#4a6cc3',
+    2: '#4ca1de',
+    3: '#94d3cc',
+    4: '#4cb8b8',
+    5: '#6ed5eb',
+    6: '#d092e2',
+    7: '#817dce',
+    8: '#b9d58c',
+    9: '#e6d267',
+    10: '#e2b765',
 };
 
 export const COLORS = {
