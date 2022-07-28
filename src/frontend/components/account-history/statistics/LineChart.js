59dbc3c5f3e488b08d4348ac176fcdbf673c14a5
padosum <makalu171@gmail.com>
2022-07-28 19:46:41 +0900

    chore ⚙️ line chart stroke 너비 수정

--- src/frontend/components/account-history/statistics/LineChart.js
+++ src/frontend/components/account-history/statistics/LineChart.js
@@ -1,18 +1,18 @@
 import GlobalStore from '../../../stores/global.js';
-import {getDateString} from '../../../utils/date.js';
-import {CATEGORY_COLORS, COLORS} from '../../../constants/colors.js';
-import {ceil} from '../../../utils/number.js';
+import { getDateString } from '../../../utils/date.js';
+import { CATEGORY_COLORS, COLORS } from '../../../constants/colors.js';
+import { ceil } from '../../../utils/number.js';
 
 export default class AccountHistoryStatisticsLineChart {
-  constructor({$parent, model, state}) {
+  constructor({ $parent, model, state }) {
     this.$target = document.createElement('canvas');
     this.$target.width = $parent.clientWidth;
     this.$target.height = 654;
     this.$target.classList.add('line-chart-canvas');
     $parent.appendChild(this.$target);
     this.ctx = this.$target.getContext('2d');
-    this.ctx.textBaseline = 'top';
-    this.ctx.lineWidth = 1;
+    this.ctx.textBaseline = 'top'
+    this.ctx.lineWidth = 1.5;
     this.xStart = 42;
     this.yStart = 36;
     this.titleHeight = 60;
@@ -32,20 +32,20 @@ export default class AccountHistoryStatisticsLineChart {
     this.render();
   }
 
-  drawText({fontWeight = 400, fontSize = '12px', color = COLORS.TITLE_ACTIVE, value, x, y}) {
+  drawText({ fontWeight = 400, fontSize = '12px', color = COLORS.TITLE_ACTIVE, value, x, y }) {
     this.ctx.font = `${fontWeight} ${fontSize} Noto Sans KR`;
     this.ctx.fillStyle = color;
     this.ctx.fillText(value, x, y);
   }
 
   drawTitle() {
-    const {categoryId} = GlobalStore.get('statisticsState');
-    const {expenditure} = this.model.get('categories');
-    const {title: categoryTitle} = expenditure.find(v => v.id === categoryId);
-    this.drawText({fontSize: '24px', value: `${categoryTitle} 카테고리 소비 추이`, x: this.xStart, y: this.yStart});
+    const { categoryId } = GlobalStore.get('statisticsState');
+    const { expenditure } = this.model.get('categories');
+    const { title: categoryTitle } = expenditure.find(v => v.id === categoryId);
+    this.drawText({ fontSize: '24px', value: `${categoryTitle} 카테고리 소비 추이`, x: this.xStart, y: this.yStart });
   }
 
-  drawStrokePath({drawPath, color = COLORS.BACKGROUND}) {
+  drawStrokePath({ drawPath, color = COLORS.BACKGROUND }) {
     this.ctx.beginPath();
     this.ctx.strokeStyle = color;
     drawPath();
@@ -53,7 +53,7 @@ export default class AccountHistoryStatisticsLineChart {
     this.ctx.closePath();
   }
 
-  drawDot({color = COLORS.PRIMARY1, x, y, radius}) {
+  drawDot({ color = COLORS.PRIMARY1, x, y, radius }) {
     this.ctx.beginPath();
     this.ctx.fillStyle = color;
     this.ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
@@ -61,7 +61,7 @@ export default class AccountHistoryStatisticsLineChart {
     this.ctx.closePath();
   }
 
-  drawGridLine({fromX, fromY, toX, toY}) {
+  drawGridLine({ fromX, fromY, toX, toY }) {
     const halfLineWidth = this.ctx.lineWidth / 2;
     this.ctx.moveTo(fromX + halfLineWidth, fromY + halfLineWidth);
     this.ctx.lineTo(toX + halfLineWidth, toY + halfLineWidth);
@@ -71,7 +71,7 @@ export default class AccountHistoryStatisticsLineChart {
     const x = this.xStart;
     let y = this.gridYStart;
     for (let i = 0; i < this.lineCnt; i += 1) {
-      this.drawGridLine({fromX: x, fromY: y, toX: x + this.contentWidth, toY: y});
+      this.drawGridLine({ fromX: x, fromY: y, toX: x + this.contentWidth, toY: y });
       y += this.dy;
     }
   }
@@ -80,7 +80,7 @@ export default class AccountHistoryStatisticsLineChart {
     let x = this.xStart;
     const y = this.gridYStart;
     for (let i = 0; i < this.lineCnt; i += 1) {
-      this.drawGridLine({fromX: x, fromY: y, toX: x, toY: y + this.gridHeight});
+      this.drawGridLine({ fromX: x, fromY: y, toX: x, toY: y + this.gridHeight });
       x += this.dx;
     }
   }
@@ -95,8 +95,8 @@ export default class AccountHistoryStatisticsLineChart {
   }
 
   drawColumnLabel() {
-    const {year, month} = GlobalStore.get('globalState');
-    const date = new Date(getDateString({year, month, date: 1}));
+    const { year, month } = GlobalStore.get('globalState');
+    const date = new Date(getDateString({ year, month, date: 1 }));
     date.setMonth(date.getMonth() - 5);
     const startYear = date.getFullYear();
     const startMonth = date.getMonth() + 1;
@@ -115,7 +115,7 @@ export default class AccountHistoryStatisticsLineChart {
     });
 
     const dc = this.dx - 0.5 * this.ctx.lineWidth;
-    this.columnData.forEach(({month, firstMonth, year}, index) => {
+    this.columnData.forEach(({ month, firstMonth, year }, index) => {
       this.drawText({
         fontWeight: 700,
         color: COLORS.LABEL,
@@ -136,14 +136,14 @@ export default class AccountHistoryStatisticsLineChart {
 
   drawGraph() {
     const categoryRecentMonthly = this.model.get('categoryRecentMonthly');
-    const maxTotal = Math.max(...categoryRecentMonthly.map(({total}) => total));
+    const maxTotal = Math.max(...categoryRecentMonthly.map(({ total }) => total));
     const digit = String(maxTotal).length - 1;
     const maxYValue = ceil(maxTotal, digit);
-    const dots = this.columnData.slice(0, 6).reduce((acc, {month}, index) => {
+    const dots = this.columnData.slice(0, 6).reduce((acc, { month }, index) => {
       const currMonthData = categoryRecentMonthly.find(v => v.month === month);
       const x = this.xStart + this.ctx.lineWidth + this.dx * index;
       const y = this.gridYStart + this.gridHeight * (currMonthData ? 1 - currMonthData.total / maxYValue : 1);
-      this.drawDot({x, y, radius: 4, color: this.categoryColor});
+      this.drawDot({ x, y, radius: 4, color: this.categoryColor });
       this.drawText({
         fontWeight: 700,
         color: COLORS.BODY,
