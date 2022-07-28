472c6d25b58024be06a00cc51c47fcd911e9b012
padosum <makalu171@gmail.com>
2022-07-28 20:56:33 +0900

    bugfix 🐛 popstate 시 라우팅 처리가 제대로 되지 않는 문제 해결

--- src/frontend/Router.js
+++ src/frontend/Router.js
@@ -1,19 +1,20 @@
 const getRouter = () => {
   const getPathname = () => {
-    const {pathname} = window.location;
+    const { pathname } = window.location;
     if (pathname === '/calendar' || pathname === '/statistics') return pathname;
     return '/';
   };
 
   const state = {};
 
-  function Router() {}
+  function Router() { }
   Router.prototype.init = (key, defaultValue) => {
     if (key in state) throw Error('이미 존재하는 key값 입니다.');
     state[key] = {
       state: defaultValue,
       observers: new Set(),
     };
+    window.history.pushState({}, null, defaultValue);
     return key;
   };
   Router.prototype.subscribe = (key, observer) => {
@@ -26,10 +27,10 @@ const getRouter = () => {
     if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
     return state[key].state;
   };
-  Router.prototype.set = (key, newValue) => {
+  Router.prototype.set = (key, newValue, pushState = true) => {
     if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
     state[key].state = newValue;
-    window.history.pushState({}, null, newValue);
+    if (pushState) window.history.pushState({}, null, newValue);
     Router.prototype.notify(key);
   };
 
@@ -37,9 +38,8 @@ const getRouter = () => {
 
   router.init('pathname', getPathname());
 
-  window.addEventListener('popstate', () => {
-    router.set('pathname', getPathname());
-  });
+  window.addEventListener('popstate', () =>
+    router.set('pathname', getPathname(), false));
 
   return router;
 };
