26776f70a3a66d5bc762de2611b75da558222f9d
gywlsp <gywls517@gmail.com>
2022-07-28 10:07:56 +0900

    refactor ♻️ router 객체 생성 방식을 object literal에서 prototype을 이용한 생성자를 이용하도록 변경

--- src/frontend/Router.js
+++ src/frontend/Router.js
@@ -4,34 +4,36 @@ const getRouter = () => {
     if (pathname === '/calendar' || pathname === '/statistics') return pathname;
     return '/';
   };
+
   const state = {};
 
-  const router = {
-    init(key, defaultValue) {
-      if (key in state) throw Error('이미 존재하는 key값 입니다.');
-      state[key] = {
-        state: defaultValue,
-        observers: new Set(),
-      };
-      return key;
-    },
-    get(key) {
-      if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
-      return state[key].state;
-    },
-    set(key, newValue) {
-      if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
-      state[key].state = newValue;
-      window.history.pushState({}, null, newValue);
-      this.notify(key);
-    },
-    subscribe(key, observer) {
-      state[key].observers.add(observer);
-    },
-    notify(key) {
-      state[key].observers.forEach(observer => observer());
-    },
+  function Router() {}
+  Router.prototype.init = (key, defaultValue) => {
+    if (key in state) throw Error('이미 존재하는 key값 입니다.');
+    state[key] = {
+      state: defaultValue,
+      observers: new Set(),
+    };
+    return key;
   };
+  Router.prototype.subscribe = (key, observer) => {
+    state[key].observers.add(observer);
+  };
+  Router.prototype.notify = key => {
+    state[key].observers.forEach(observer => observer());
+  };
+  Router.prototype.get = key => {
+    if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
+    return state[key].state;
+  };
+  Router.prototype.set = (key, newValue) => {
+    if (!(key in state)) throw Error('존재하지 않는 key값 입니다.');
+    state[key].state = newValue;
+    window.history.pushState({}, null, newValue);
+    Router.prototype.notify(key);
+  };
+
+  const router = new Router();
 
   router.init('pathname', getPathname());
 
